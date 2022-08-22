353d0015840897a2c1d83a67231fdf7123ddd3ed
Marcelo Silvero <msilvero@sancristobal.local>
2022-08-22 08:30:15 -0400

    agregado async a _document

--- fernando-herrera/pages/_document.js
+++ fernando-herrera/pages/_document.js
@@ -27,26 +27,26 @@ export default class MyDocument extends Document {
             <body className="sidebar-mini">
                 <Main />
                 <NextScript />
-                <script src="/components/jquery/jquery3.4.1/jquery-3.4.1.js"></script>
-                <script src="/components/bootstrap/js/bootstrap.min.js"></script>
-                <script src="/components/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-                <script src="/components/bootstrap-datepicker/locales/bootstrap-datepicker.es.min.js"></script>
-                <script src="/plugins/moment/moment-with-locales.min.js"></script>
-                <script src="/plugins/datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
-                <script src="/components/excel/js/xlsx.full.min.js"></script>
-                <script src="/components/fileinput/js/fileinput.min.js"></script>
-                <script src="/components/fileinput/themes/explorer/theme.min.js"></script>
-                <script src="/components/fileinput/js/locales/es.js"></script>
-                <script src="/components/moment/min/moment.min.js"></script>        
-                <script src="/components/moment/min/locales.js"></script>
-                <script src="/components/footables-boostrap/footable-3.1.5/js/footable.min.js"></script>
-                <script src="/components/select-bootstrap/select-bootstrap1.12.4/js/bootstrap-select.min.js"></script>
-                <script src="/components/inputmask/inputmask@4.0.0/dist/jquery.inputmask.bundle.min.js"></script>
-                <script src="/components/inputmask/inputmask@4.0.0/dist/min/inputmask/phone-codes/phone.min.js"></script>
-                <script src="/components/exif-js/exif.js"></script>
-                <script src="/components/sweetalert2@9/sweetalert2@9.js"></script>
-                <script src="/components/sweetalert2@9/sweetalert2@9.js"></script>
-                <script src="/js/funciones.js" type="text/javascript"></script>        
+                <script src="/components/jquery/jquery3.4.1/jquery-3.4.1.js" async></script>
+                <script src="/components/bootstrap/js/bootstrap.min.js" async></script>
+                <script src="/components/bootstrap-datepicker/js/bootstrap-datepicker.js" async></script>
+                <script src="/components/bootstrap-datepicker/locales/bootstrap-datepicker.es.min.js" async></script>
+                <script src="/plugins/moment/moment-with-locales.min.js" async></script>
+                <script src="/plugins/datetimepicker/js/bootstrap-datetimepicker.min.js" async></script>
+                <script src="/components/excel/js/xlsx.full.min.js" async></script>
+                <script src="/components/fileinput/js/fileinput.min.js" async></script>
+                <script src="/components/fileinput/themes/explorer/theme.min.js" async></script>
+                <script src="/components/fileinput/js/locales/es.js" async></script>
+                <script src="/components/moment/min/moment.min.js" async></script>        
+                <script src="/components/moment/min/locales.js" async></script>
+                <script src="/components/footables-boostrap/footable-3.1.5/js/footable.min.js" async></script>
+                <script src="/components/select-bootstrap/select-bootstrap1.12.4/js/bootstrap-select.min.js" async></script>
+                <script src="/components/inputmask/inputmask@4.0.0/dist/jquery.inputmask.bundle.min.js" async></script>
+                <script src="/components/inputmask/inputmask@4.0.0/dist/min/inputmask/phone-codes/phone.min.js" async></script>
+                <script src="/components/exif-js/exif.js" async></script>
+                <script src="/components/sweetalert2@9/sweetalert2@9.js" async></script>
+                <script src="/components/sweetalert2@9/sweetalert2@9.js" async></script>
+                <script src="/js/funciones.js" type="text/javascript" async></script>        
             </body>
         </html>
     );
